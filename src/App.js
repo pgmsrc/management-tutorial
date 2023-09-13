18af2b8311965b7a7bb091daab670805ef351fe2
pgmsrc <pgmsrc@gmail.com>
2023-09-13 12:24:08 +1000

    Created Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://loremflickr.com/64/64/1',
+  'name': '서웅교',
+  'birthday': '700429',
+  'gender': '남자',
+  'job': '무직'
+  },
+  {
+    'id': 2,
+    'image': 'https://loremflickr.com/64/64/2',
+    'name': '김지애',
+    'birthday': '761112',
+    'gender': '여자',
+    'job': '메니저'
+    },
+  {
+  'id': 3,
+  'image': 'https://loremflickr.com/64/64/3',
+  'name': '서준혁',
+  'birthday': '030318',
+  'gender': '남자',
+  'job': '대학생'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's start management system!</h2>
+    <div>
+      {customers.map(customer => {
+        return (
+          <Customer key={customer.id}
+          id={customer.id}
+          image={customer.iamge}
+          name={customer.name}
+          birthday={customer.borthday}
+          gender={customer.gender}
+          job={customer.job}
+          />
+        )
+      })}
     </div>
   );
 }
