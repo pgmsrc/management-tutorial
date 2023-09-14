0194b58f3a5f84276ccbb4ab576bbf01330c042f
pgmsrc <pgmsrc@gmail.com>
2023-09-14 20:35:19 +1000

    NodeJS DB connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Customer from './components/Customer';
-import City from './components/City';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
@@ -9,24 +8,15 @@ import TableCell from '@mui/material/TableCell';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const url1 = "http://localhost:5000/api/customers";
-  const url2 = "http://localhost:5000/api/city";
-  const [data1, setData] = useState([]);
-  const [data2, setData2] = useState([]);
+  const url = "http://localhost:5000/api/customers";
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch(url1)
+    fetch(url)
     .then(response => response.json())
-    .then(data1 => setData(data1))
+    .then(data => setData(data))
     .catch(error => console.error(error));
-  }, [url1])
-
-  useEffect(() => {
-    fetch(url2)
-    .then(response => response.json())
-    .then(data2 => setData2(data2))
-    .catch(error => console.error(error));
-  }, [url2])
+  }, [url])
 
   return (
     <Paper> 
@@ -42,27 +32,10 @@ function App() {
           </TableRow>
         </TableHead>
       </Table>
-        {data1.map((customer) => { return (<Customer key={customer.id}
+        {data.map((customer) => { return (<Customer key={customer.id}
           id={customer.id} image={customer.image} name={customer.name} birthday={customer.birthday} gender={customer.gender} job={customer.job}
           />);
         })}
-
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">ID</TableCell>
-            <TableCell align="left">Name</TableCell>
-            <TableCell align="left">ContryCode</TableCell>
-            <TableCell align="left">District</TableCell>
-            <TableCell align="left">Population</TableCell>
-          </TableRow>
-        </TableHead>
-      </Table>
-        {data2.map((city) => { return (<City key={city.ID}
-          id={city.ID} name={city.Name} countryCode={city.CountryCode} distrcit={city.District} population={city.Population}
-          />);
-        })}
-
     </Paper>
   );
 }
--- server.js
+++ server.js
@@ -9,41 +9,11 @@ const mysql = require('mysql');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
-app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://loremflickr.com/64/64/1',
-            'name': '김지애',
-            'birthday': '761112',
-            'gender': '여자',
-            'job': '메니저'
-            },
-          {
-            'id': 2,
-            'image': 'https://loremflickr.com/64/64/2',
-            'name': '서준혁',
-            'birthday': '030318',
-            'gender': '남자',
-            'job': '대학생'
-          },
-          {
-            'id': 3,
-            'image': 'https://loremflickr.com/64/64/3',
-            'name': '서웅교',
-            'birthday': '700429',
-            'gender': '남자',
-            'job': '개발자'
-          }
-    ]);
-});
-
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 
-app.get('/api/city', function(req, res){
-    const db = mysql.createConnection({
+app.get('/api/customers', function(req, res){
+    const connection = mysql.createConnection({
         host: conf.host,
         user: conf.user, 
         port: conf.port,
@@ -51,12 +21,11 @@ app.get('/api/city', function(req, res){
         database : conf.database
     });
     
-    const sqlQuery = 'select * from world.city where CountryCode = "KOR" and District = "Kyonggi"';
-    db.query(sqlQuery, function (err, result) {
+    const sqlQuery = 'select * from management.customer';
+    connection.query(sqlQuery, function (err, result, field) {
         console.log('result',result);
         res.send(result);
     });
 });
-
 
 app.listen(port, () => console.log(`Listeing on port: ${port}`));
